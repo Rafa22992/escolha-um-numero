4326418adf5e0802687c79873830ed523d99d3d8
Rafael Pinheiro <rafael_dev22992@hotmail.com>
2023-04-28 19:10:51 -0300

    Update player.py

--- escolha_um_numero/player.py
+++ escolha_um_numero/player.py
@@ -1,37 +1,58 @@
 from random import choice
+import os
 
 def advinhe_num():
+    
     numeros = []
     bordas = '-' * 20
+    print(bordas)
+    print(f'{"ADVINHE O NÚMERO":=^20}')
+    print(bordas)
 
-    def quant_num_range():
-        quant_num = input('Escolha a quantidade de numeros: ')
-        print(bordas)
-        print(f'Muito bem, você escolheu {quant_num} números !!!')
-        print(bordas)
-        int_num_quant = int(quant_num)
+    while True:
+            quant_num = input('Escolha a quantidade de numeros: ')
+            print(bordas)
+            os.system('cls')
 
-        def embaralha_nums():
-            numeros_range = range(1, int_num_quant + 1)
+            if quant_num.isalpha():
+                 print('Digite apenas números!!!')
+                 print(bordas)
+                 continue
             
+            print(f'Muito bem, você escolheu {quant_num} números !!!')
+            print(bordas)
+            int_num_quant = int(quant_num)
+
+            numeros_range = range(1, int_num_quant + 1)
+                
             for n in numeros_range:
                 numeros.append(n)
                 emb_num = choice(numeros)
-                
+                    
             num_escolhido = input('Advinhe o número: ')
             print(bordas)
+            os.system('cls')
+
+            if num_escolhido.isalpha():
+                 print('Digite apenas números!!!')
+                 print(bordas)
+                 continue
 
             int_num_esc = int(num_escolhido)
+
+            if int_num_esc > len(numeros):
+                print(f'Você só pode escolher até o {quant_num}')
+                print(bordas)
+                continue
 
             if int_num_esc == emb_num:
                 print(f'PARÁBENS VOCÊ ACERTOU O NÚMERO QUE É ({emb_num})!!!')
                 print(bordas)
+                continue
 
             else:
                 print(f'AAAAAAH, você errou, o número correto era ({emb_num})')
                 print(bordas)
 
-        embaralha_nums()   
-    quant_num_range()
-
+            
 advinhe_num()
